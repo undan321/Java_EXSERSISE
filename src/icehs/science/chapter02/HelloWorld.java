c84092e7fe73c979cf5c865cc74d8dd8b1742f34
admin <admin@10.137.138.156>
2018-01-02 17:06:10 +0900

    Chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,8 +1,10 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	//나의 첫번째 java 프로그램
+	
 	public static void main(String[] args) {
+
 		System.out.println("Hello World~~");
 		System.out.println("안녕하세요");
 	}
