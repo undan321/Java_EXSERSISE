768a01885bc8afae404efe156b4702ff77e577e2
admin <admin@10.137.138.156>
2018-01-02 17:50:05 +0900

    Chapter03

---
+++ src/icehs/science/chapter03/TestScore.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter03;
+
+public class TestScore {
+
+	public static void main(String[] args) {
+		//A그룹에 입사한 홍길동 사원의 sw개발 성적을 출력하는 프로그램
+		int javaScore = 100;
+		int sql = 80;
+		int s_J = 85;
+		System.out.println(javaScore);
+		System.out.println(sql);
+		System.out.println(s_J);
+
+	}
+
+}
